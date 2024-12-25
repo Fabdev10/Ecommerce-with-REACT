f8b37429b01dc2fa7cf60e3a0a10f96c3d32ad41
Fabio Mazzà <161578227+Fabdev10@users.noreply.github.com>
2024-12-25 16:13:04 +0100

    Add files via upload

---
+++ src/Components/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="hero border-1 pb-3">
+        <div className="card bg-dark text-white border-0 mx-3">
+            <div className="container">
+              <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
+            </div>
+          </div>
+        </div>
+    </>
+  );
+};
+
+export default Home;
